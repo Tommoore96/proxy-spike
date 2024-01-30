39569c3dc56f5e3c4a68a2b94805ea0b71e0e321
Tom Moore <tommoore.dev@gmail.com>
2024-01-30 16:56:30 +0000

    whitespace change

---
+++ apps/next/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // async rewrites() {
+  //   return [{ source: "/test", destination: "http://localhost:5001/" }];
+  // },
+};
+
+export default nextConfig;
---
+++ apps/next/src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  if (request.headers.get("x-forwarded-host")?.startsWith("listen.")) {
+    return NextResponse.rewrite(
+      new URL(request.nextUrl.pathname, "http://localhost:5001/")
+    );
+  }
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
