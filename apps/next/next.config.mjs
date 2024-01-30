918a2ab76c19568d5875c8436369b509e8b7a0de
Tom Moore <tommoore.dev@gmail.com>
2024-01-30 17:22:44 +0000

    remove next config

--- apps/next/next.config.mjs
+++ apps/next/next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // async rewrites() {
-  //   return [{ source: "/test", destination: "http://localhost:5001/" }];
-  // },
-};
+const nextConfig = {};
 
 export default nextConfig;
